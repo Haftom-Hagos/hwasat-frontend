f8ef599fab329d3caa7bff32ace056ef3a3d0f86
Haftom-Hagos <136759437+Haftom-Hagos@users.noreply.github.com>
2025-12-09 13:49:45 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ export default function Navbar(){
       <div className="w-full bg-gray-800 max-w-none px-4 md:px-6 lg:px-8 py-3 flex items-center justify-between">
         
         <div className="flex items-center gap-3">
-          <img 
-            {/*src={logo}*/} 
+          {/*<img 
+            src={logo} 
             alt="Logo"
             className="w-12 h-12 object-contain"  
-          />
+          />*/}
           <div className="text-xl font-semibold text-white">Hwasat Geosense</div>
         </div>
 
